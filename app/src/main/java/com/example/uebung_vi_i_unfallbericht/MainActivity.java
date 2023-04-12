bcee4ef2a50ef3929709157a3f6e12c64ce1079b
Terminator15474 <jandommert8@gmail.com>
2023-04-12 13:46:19 +0200

    first layout

--- app/src/main/java/com/example/uebung_vi_i_unfallbericht/MainActivity.java
+++ app/src/main/java/com/example/uebung_vi_i_unfallbericht/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.uebung_vi_i_unfallbericht;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener((v) -> {
+            Intent intent = new Intent(this, UserInputActivity.class);
+            startActivity(intent);
+        });
     }
 }
