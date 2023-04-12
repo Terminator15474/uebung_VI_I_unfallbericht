8e2ebb292d2b2a1f6b35876875b8396689a03525
Terminator15474 <jandommert8@gmail.com>
2023-04-12 13:35:35 +0200

    init commit

---
+++ app/src/main/java/com/example/uebung_vi_i_unfallbericht/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.uebung_vi_i_unfallbericht;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
